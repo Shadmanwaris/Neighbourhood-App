c566635261de12bff2a677aa2d5d975c2fbf9c86
Shadman Waris <shadin4u@gmail.com>
2017-05-21 01:37:17 +0530

    updated

---
+++ js/model.js
@@ -0,0 +1,96 @@
+//*************************
+//******* Model ***********
+//*************************
+
+var locations = [
+    {
+        category : "Museum",
+        name : "The Metropolitan Museum",
+        website : "metmuseum.org",
+        address : "5th Ave, New York,",
+        location : {
+            lat : 40.795126,
+            lng : -73.963591
+        }
+    },
+    {
+        category : "Museum",
+        name : "Museum of Modern Art",
+        website : "moma.org",
+        address : " 53rd St, New York",
+        location : {
+            lat : 40.776410,
+            lng : -73.978010
+        }
+    },
+    {
+        category : "Museum",
+        name : "The Paley Center For Media",
+        website : "paleycenter.org",
+        address : "52nd St, New York",
+        location : {
+            lat : 40.776929,
+            lng : -73.976637
+        }
+    },
+    {
+        category : "Restaurant",
+        name : "Pollo Campero",
+        website : "campero.com",
+        address : "Bergenline Ave, West New York",
+        location : {
+            lat : 40.807951,
+            lng :  -74.013030
+        }
+    },
+    {
+        category : "Restaurant",
+        name : "The Palm Too",
+        website : "thepalm.com",
+        address : " 2nd Ave, New York",
+        location : {
+            lat : 40.766880,
+            lng : -73.970458
+        }
+    },
+    {
+        category : "Restaurant",
+        name : "Red Lobster",
+        website : "redlobster.com",
+        address : "Xavier Dr, Yonkers",
+        location : {
+            lat : 40.945016,
+            lng : -73.850295
+        }
+    },
+    {
+        category : "Cafe",
+        name : "The Brooklyn Cat Cafe",
+        website : "catcafebk.com",
+        address : "Atlantic Ave, Brooklyn",
+        location : {
+            lat : 40.706358,
+            lng : -73.996550
+        }
+    },
+    {
+        category : "Cafe",
+        name : "969 NYC Coffee",
+        website : "zomato.com",
+        address : "80th St, Jackson Heights",
+        location : {
+            lat : 40.764110,
+            lng : -73.886687
+        }
+    },
+    {
+        category : "Cafe",
+        name : "Society Cafe",
+        website : "societycafenyc.com",
+        address : "13th St, New York",
+        location : {
+            lat : 40.753188,
+            lng : -73.997924
+        }
+    }
+    ];
